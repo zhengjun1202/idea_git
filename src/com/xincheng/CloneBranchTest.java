30deea19c9049b302d685b976b78b924315d93c9
郑军 <zj749122147@outlook.com>
2018-11-18 20:00:56 +0800

    尝试从git的分支项目上面拉取代码

---
+++ src/com/xincheng/CloneBranchTest.java
@@ -0,0 +1,17 @@
+package com.xincheng;
+
+/**
+ * ClassName:CloneBranchTest
+ * Package:com.xincheng
+ * Description:
+ *
+ * @Date:2018/11/18 19:59
+ * @Author: 郑军
+ */
+public class CloneBranchTest {
+
+    public static void main(String[] args) {
+        System.out.println("测试从githup上面拉取分支项目");
+    }
+
+}
