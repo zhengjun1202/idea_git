d4c5f9f394e42b18343050bda19a4f69bcb1008a
郑军 <zj749122147@outlook.com>
2018-11-18 18:06:00 +0800

    idea开发工具中测试，使用git进行项目的管理

---
+++ src/com/xincheng/gitTest.java
@@ -0,0 +1,17 @@
+package com.xincheng;
+
+/**
+ * ClassName:gitTest
+ * Package:com.xincheng
+ * Description:
+ *
+ * @Date:2018/11/18 18:02
+ * @Author: 郑军
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("在idea中使用git上传数据信息");
+    }
+
+}
