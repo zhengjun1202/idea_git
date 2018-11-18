dbd5491f6e5cc686bd6921398f053f5c52f31a81
郑军 <zj749122147@outlook.com>
2018-11-18 18:12:16 +0800

    新添加一个信息并上传到gitHup中

---
+++ src/com/xincheng/AddNewMessage.java
@@ -0,0 +1,17 @@
+package com.xincheng;
+
+/**
+ * ClassName:AddNewMessage
+ * Package:com.xincheng
+ * Description:
+ *
+ * @Date:2018/11/18 18:09
+ * @Author: 郑军
+ */
+public class AddNewMessage {
+
+    public static void main(String[] args) {
+        System.out.println("添加一条新的信息，并上传到githup中");
+    }
+
+}
