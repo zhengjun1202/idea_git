afa9d89aad068e12657ba729cb402fab05defc45
郑军 <zj749122147@outlook.com>
2018-11-18 20:01:42 +0800

    修改了类名

--- src/com/xincheng/GitTest.java
+++ src/com/xincheng/GitTest.java
@@ -1,14 +1,14 @@
 package com.xincheng;
 
 /**
- * ClassName:gitTest
+ * ClassName:GitTest
  * Package:com.xincheng
  * Description:
  *
  * @Date:2018/11/18 18:02
  * @Author: 郑军
  */
-public class gitTest {
+public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("在idea中使用git上传数据信息");
